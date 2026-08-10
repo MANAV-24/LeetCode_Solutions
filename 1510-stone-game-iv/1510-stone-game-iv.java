bfd99c00aae6bc6a117cab09b6527bffdd7438b2
MANAV-24 <128450078+MANAV-24@users.noreply.github.com>
2026-08-10 22:06:17 +0530

    Time: 15 ms (64.49%), Space: 42.2 MB (90.34%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,26 +1,19 @@
 class Solution {
-    int[] t;
+    public boolean winnerSquareGame(int n) {
+        boolean[] t = new boolean[n+1];
+        Arrays.fill(t, false);
 
-    public boolean solve(int n){
-        if(n == 0){
-            return false;
-        }
+        t[0] = false;
+        
+        for(int i=1; i < n+1; i++){
 
-        if(t[n] != -1){
-            return t[n] == 1 ? true : false;
-        }
-        for(int k=1; k*k <= n;k++){
-            if(solve(n - (k*k)) == false){
-                t[n] = 1;
-                return true;
+            for(int k=1; k*k <= i; k++){
+                if(t[i - (k*k)] == false){
+                    t[i] = true;
+                    break;
+                }
             }
         }
-        t[n] = 0;
-        return false;
-    }
-    public boolean winnerSquareGame(int n) {
-        t =new int[n+1];
-        Arrays.fill(t, -1);
-        return solve(n);
+        return t[n];
     }
 }
