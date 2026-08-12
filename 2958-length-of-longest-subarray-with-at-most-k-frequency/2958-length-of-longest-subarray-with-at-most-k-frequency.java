20cabd5595cd55b6d3bee84586a7367d027b5fd6
MANAV-24 <128450078+MANAV-24@users.noreply.github.com>
2026-08-13 00:37:39 +0530

    Time: 69 ms (43.18%), Space: 88.8 MB (74.81%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -6,16 +6,25 @@ class Solution {
         int i = 0;
         int j = 0;
         int result = 0;
+        int culprit = 0; 
         
         while(j < n){
             mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
 
-            while(i < j && mp.get(nums[j]) > k){
+            if(mp.get(nums[j]) == k+1){
+                culprit++;
+            }
+            if(culprit > 0){
                 mp.put(nums[i], mp.get(nums[i]) - 1);
+                if(mp.get(nums[i]) == k){
+                    culprit--;
+                }
                 i++;
             }
 
-            result = Math.max(result, j-i+1);
+            if(culprit == 0){
+                result = Math.max(result, j-i+1);
+            }
             j++;
         }
         return result;
