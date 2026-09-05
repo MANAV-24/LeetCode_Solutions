9d67193ecea636834f0fc7c88bd2db321fff684a
MANAV-24 <128450078+MANAV-24@users.noreply.github.com>
2026-09-06 00:08:12 +0530

    Time: 3 ms (100%), Space: 133.4 MB (14.89%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,28 +1,24 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        
-        int[] prefixMax = new int[n];
-        int[] suffixMin = new int[n];
 
-        prefixMax[0] = nums[0];
+        int[] minFromIndex = new int[n];
+        int minEl = Integer.MAX_VALUE;
 
-        for(int i=1; i<n; i++){
-            prefixMax[i] = Math.max(prefixMax[i-1], nums[i]);
-        }        
-
-        suffixMin[n-1] = nums[n - 1];
-
-        for(int i=n-2; i>=0; i--){
-            suffixMin[i] = Math.min(suffixMin[i+1], nums[i]);
+        for(int i = n-1; i>=0; i--){
+            minEl = Math.min(minEl, nums[i]);
+            minFromIndex[i] = minEl;
         }
 
+        int maxEl = Integer.MIN_VALUE;
         for(int i=0; i<n; i++){
-            if(prefixMax[i] - suffixMin[i] <= k){
+            maxEl = Math.max(maxEl, nums[i]);
+
+            if(maxEl - minFromIndex[i] <= k){
                 return i;
             }
         }
-
+        
         return -1;
     }
 }
