84c7a73da6b4abdec0a13f136318160ff21f6a1a
MANAV-24 <128450078+MANAV-24@users.noreply.github.com>
2026-09-24 00:47:01 +0530

    Time: 5 ms (97.39%), Space: 44.1 MB (48.47%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if(n > m){
+            return false;
+        }
+        
+        int[] s1_freq = new int[26];
+        int[] s2_freq = new int[26];
+
+        for(char ch : s1.toCharArray()){
+            s1_freq[ch - 'a']++;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while(j < m){
+            s2_freq[s2.charAt(j) - 'a']++;
+
+            if(j-i+1 > n){
+                s2_freq[s2.charAt(i) - 'a']--;
+                i++;
+            }
+
+            if(Arrays.equals(s1_freq, s2_freq)){
+                return true;
+            }
+            j++;
+        }
+
+        return false;
+    }
+}
